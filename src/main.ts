a2b4b7654984b42f15bc0eef03eab20343f4e684
Étienne Doré <119957024+etiennedo@users.noreply.github.com>
2024-08-31 22:36:01 -0400

    [feat] now with search!

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,15 @@ import Dexie, { type EntityTable } from 'dexie';
 interface Note {
     id: number;
     content: string;
-  }
+}
 
 class Note {
     id: number
     content: string
-  constructor(id: number, content: string) {
-    this.id = id;
-    this.content = content;
-  }
+    constructor(id: number, content: string) {
+        this.id = id;
+        this.content = content;
+    }
 }
 
 const noteContent = document.getElementById("noteContent") as HTMLTextAreaElement;
@@ -19,58 +19,57 @@ let noteId: number | null = null;
 
 // Save and delete buttons
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('JavaScript is working!');
+    console.log('JavaScript is working!');
 
-  const saveButton: HTMLElement = document.getElementById("save") as HTMLButtonElement;
-  saveButton.addEventListener("click", () => {
-    
-    saveNote(noteContent.value);
-    
-    console.log("note saved with button!");
-  });
+    const saveButton: HTMLElement = document.getElementById("save") as HTMLButtonElement;
+    saveButton.addEventListener("click", () => {
+        saveNote(noteContent.value);
+        console.log("note saved with button!");
+    });
 
-  document.addEventListener("keydown", (event) => {
-    if ((event.metaKey || event.ctrlKey) && event.key === "s") {
-      event.preventDefault(); // Prevent the default "Save Page" action
-      saveNote(noteContent.value);
-      console.log("note saved with Cmd + S / Ctrl + S!");
-    }
-  });
+    document.addEventListener("keydown", (event) => {
+        if ((event.metaKey || event.ctrlKey) && event.key === "s") {
+            event.preventDefault(); // Prevent the default "Save Page" action
+            saveNote(noteContent.value);
+            console.log("note saved with Cmd + S / Ctrl + S!");
+        }
+    });
 
-  const deleteButton: HTMLElement = document.getElementById("delete") as HTMLButtonElement;
-  deleteButton.addEventListener("click", () => {
-    if (noteId !== null) {
-    deleteNote(noteId);
-    }
-    noteContent.value = "";
-    console.log("note deleted with button!");
-  });
+    const deleteButton: HTMLElement = document.getElementById("delete") as HTMLButtonElement;
+    deleteButton.addEventListener("click", () => {
+        if (noteId !== null) {
+            deleteNote(noteId);
+        }
+        noteContent.value = "";
+        console.log("note deleted with button!");
+    });
 
-  const newButton: HTMLElement = document.getElementById("new") as HTMLButtonElement;
-  newButton.addEventListener("click", () => {
-  newNote()
-  console.log("new note started!");
-  });
+    const newButton: HTMLElement = document.getElementById("new") as HTMLButtonElement;
+    newButton.addEventListener("click", () => {
+        newNote();
+        console.log("new note started!");
+    });
+
+    const searchBar = document.getElementById("search") as HTMLInputElement;
+    searchBar?.addEventListener("input", () => {
+            const searchTerm = searchBar.value.trim();
+            searchNotes(searchTerm);
+    });
 });
 
 // Create DB
 const db = new Dexie("NotesDatabase") as Dexie & {
-    notes: EntityTable<
-    Note,
-    'id'
-    >;
+    notes: EntityTable<Note, 'id'>;
 };
 db.version(1).stores({
-  notes: "++id, content"
+    notes: "++id, content"
 });
 
 // New note to db
 const newNote = async () => {
-  noteContent.value = "";
-  noteId = null;
-  /* const id = await db.notes.add({ content: "" });
-  noteId = id;
-  getAllNotes(); */
+    noteContent.value = "";
+    noteId = null;
+    await getAllNotes(); // Update the list of all notes
 };
 
 // Save note to DB
@@ -78,31 +77,28 @@ const saveNote = async (content: string) => {
     if (noteId !== null && noteContent !== null) {
         await db.notes.update(noteId, { content: content });
     } else {
-      const id = await db.notes.add({ content: content });
-      noteId = id;
+        const id = await db.notes.add({ content: content });
+        noteId = id;
     }
-    getAllNotes();
+    await getAllNotes(); // Update the list of all notes
 };
 
-//delete note
-const deleteNote = async (noteContent: number) => {
-  if (noteId !== null) {
-      await db.notes.delete(noteContent);
-      noteId = null;
-  }
-  getAllNotes();
+// Delete note
+const deleteNote = async (id: number) => {
+    if (noteId !== null) {
+        noteId = id
+        await db.notes.delete(id);
+        noteId = null;
+    }
+    await getAllNotes(); // Update the list of all notes
 };
 
-// Generate note list from DB
-async function getAllNotes() {
-  try {
-    const notes = await db.notes.toArray();
-    console.log(notes);
-
-    notes.reverse();
-
+// Render notes list
+async function renderNotes(notes: Note[]) {
     const notesList = document.getElementById("notesList") as HTMLUListElement;
     notesList.innerHTML = '';
+
+    notes.reverse();
 
     notes.forEach((note: Note) => {
         const li = document.createElement("li");
@@ -114,36 +110,41 @@ async function getAllNotes() {
             li.classList.add("selected");
             li.classList.remove("not-selected");
             noteContent.value = note.content;
-          }
+        }
 
         li.addEventListener("click", () => {
             notesList.querySelectorAll("li").forEach((item) => {
                 item.classList.remove("selected");
                 item.classList.add("not-selected");
             });
-                li.classList.add("selected")
-                li.classList.remove("not-selected")
-                noteContent.value = note.content;
-                noteId = note.id;
-        });
-    });
-  } catch (error) {
-    console.error('Error getting notes:', error);
-  }
-};
-
-// Selected note functionality
-const noteInList = document.querySelectorAll("li.not-selected");
-
-try {
-    noteInList.forEach((note) => {
-        note.addEventListener("click", () => {
-            note.classList.add("selected");
+            li.classList.add("selected");
+            li.classList.remove("not-selected");
+            noteContent.value = note.content;
+            noteId = note.id;
         });
     });
 }
-catch (error) {
-    console.error("Error gathering note list: ", error)
+
+// Get all notes
+async function getAllNotes() {
+    try {
+        const notes = await db.notes.toArray();
+        await renderNotes(notes);
+    } catch (error) {
+        console.error('Error getting notes:', error);
+    }
 }
 
-getAllNotes();
+// Search notes and render results
+async function searchNotes(keyword: string) {
+    try {
+        const searchResults = await db.notes
+            .filter(note => note.content.includes(keyword))
+            .toArray();
+        await renderNotes(searchResults);
+    } catch (error) {
+        console.error("Error querying the database:", error);
+    }
+}
+
+getAllNotes(); // Initial call to populate notes list
